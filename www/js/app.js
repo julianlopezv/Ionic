04fd2d0f26c73ac99145ca65bf2e24ae14712891
julian.lopezv@udea.edu.co <julian.lopezv@udea.edu.co>
2016-04-16 10:45:57 -0500

    First project with errors

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+angular.module('app', ['ionic', 'app.controllers', 'app.routes'])
+.run(
+  function($ionicPlatform) {
+    $ionicPlatform
+    .ready(
+      function() {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+          StatusBar.styleDefault();
+        }
+      }
+    );
+  }
+)
